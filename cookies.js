2d151d8fe7f45c484d60ab049bd4aac0cb0cc59c
andrewbethke <andrew.bethke@outlook.com>
2022-12-25 21:42:57 -0600

    Now enables scripts after cookie consent.

--- cookies.js
+++ cookies.js
@@ -1,20 +1,56 @@
-function saveCookiePreferences(){
+/**
+ * Takes any script with the type "javascript/blocked" and deletes it,
+ * recreating it with the type "application/javascript."
+ */
+function enableBlockedScripts() {
+    let scripts = Array.from(document.getElementsByTagName("script"));
+
+    for (let script of scripts) {
+        // If the script wasn't blocked, don't touch it and just move on.
+        if (script.getAttribute("type") !== "javascript/blocked")
+            continue;
+        // Otherwise:
+        // Remove the script.
+        script.remove();
+        // Change its type to allow javascript.
+        script.setAttribute("type", "application/javascript");
+        // Add the script back.
+        document.head.appendChild(script);
+    }
+}
+
+/**
+ * Hides the cookie consent form and enables scripts so the application
+ * can move on in its execution.
+ */
+function unloadCookieConsent() {
+    // Hide the cookie consent form and show the sign in form.
+    document.getElementById("cookie-consent").style.display = "none";
+    document.getElementById("google-auth").style.display = "block";
+    // Enable all of the blocked scripts.
+    enableBlockedScripts();
+}
+
+/**
+ * Runs when the "save" button is pressed in the cookie consent form.
+ * Saves preferences and then handles unloading the form.
+ */
+function saveCookiePreferences() {
     document.cookie = "functionality-cookies-enabled=" + functionalityCookieBox.checked;
     unloadCookieConsent();
 }
 
-function disableAllCookies(){
+/**
+ * Runs when the "disallow all" button is pressed in the cookie consent form.
+ * Changes the form to reflect this preference, and then acts as if the user
+ * saved their preferences.
+ */
+ function disableAllCookies() {
     functionalityCookieBox.checked = false;
     saveCookiePreferences();
 }
 
-function unloadCookieConsent(){
-    document.getElementById("cookie-consent").style.display = "none";
-    document.getElementById("google-auth").style.display = "block";
-    // TODO: Execute the disabled scripts.
-}
-
-if(document.cookie.includes("functionality-cookies-enabled")){
+if (document.cookie.includes("functionality-cookies-enabled")) {
     unloadCookieConsent();
 }
 
