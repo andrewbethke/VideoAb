83481433f4d94414675cdb8c630ee5f739b987df
andrewbethke <andrew.bethke@outlook.com>
2023-01-20 19:36:05 -0600

    Fixed borken parts of video listing

--- videoab.js
+++ videoab.js
@@ -13,15 +13,60 @@ function logError(err) {
  * in the list and sets the program's selected video ID.
  * @param {String} videoId 
  */
-function selectVideo(videoId){
+function selectVideo(videoId) {
     console.log(videoId);
     // Clear visual selection of old video.
-    if(videoAb.selected != "")
+    if (videoAb.selected != "")
         document.getElementById(videoAb.selected).removeAttribute("style");
-    
+
     // Select new video.
     videoAb.selected = videoId;
     document.getElementById(videoAb.selected).style.backgroundColor = "#CCFFCC";
+}
+
+/**
+ * Takes a video from videos.list, creates a list entry for it, and adds it to
+ * the end of the list in the HTML.
+ * @param video 
+ */
+function createVideoNode(video) {
+    // Creates the parent block for the whole list entry.
+    let div = document.createElement("div");
+    div.classList.add("videolistitem");
+    div.id = video.id;
+
+    // Adds the link the user clicks to select the video. 
+    let selectorLink = document.createElement("a");
+    selectorLink.onclick = function () { selectVideo(video.id) };
+    div.appendChild(selectorLink);
+
+    // Adds the thumbnail image.
+    let thumbnail = document.createElement("img");
+    thumbnail.src = video.snippet.thumbnails.medium.url;
+    thumbnail.classList.add("videolistthumbnail");
+    selectorLink.appendChild(thumbnail);
+
+    // Adds a div to contain all the information on the video.
+    let info = document.createElement("div");
+    selectorLink.appendChild(info);
+
+    // Adds the title.
+    let title = document.createElement("h3");
+    title.innerText = video.snippet.title;
+    info.appendChild(title);
+
+    // Adds the publish date (or upload date if the video is unlisted/private).
+    let date = document.createElement("p");
+    date.innerText = new Date(video.snippet.publishedAt).toLocaleDateString('en-us', { year: "numeric", month: "short", day: "numeric" });
+    info.appendChild(date);
+
+    // Adds a link to the video on youtube.
+    let link = document.createElement("a");
+    link.href = `http://www.youtube.com/watch?v=${video.id}`;
+    link.classList.add("videolistitemlink");
+    info.appendChild(link);
+
+    document.getElementById("videolist").appendChild(div);
 }
 
 /**
@@ -30,39 +75,11 @@ function selectVideo(videoId){
 function populateVideos(response) {
     let parsedResponse = JSON.parse(response.body);
     console.log(parsedResponse);
-    for(let video of parsedResponse.items) {
+    for (let video of parsedResponse.items) {
+        // Adds the video's response object to an object so it can be retrieved later.
         fetchedVideos[video.contentDetails.videoId] = video;
-
-        let div = document.createElement("div");
-        div.classList.add("videolistitem");
-        div.id = video.contentDetails.videoId;
-
-        let selectorLink = document.createElement("a");
-        selectorLink.onclick = function() {selectVideo(video.contentDetails.videoId)};
-        div.appendChild(selectorLink);
-
-        let thumbnail = document.createElement("img");
-        thumbnail.src = video.snippet.thumbnails.medium.url;
-        thumbnail.classList.add("videolistthumbnail");
-        selectorLink.appendChild(thumbnail);
-
-        let info = document.createElement("div");
-        selectorLink.appendChild(info);
-
-        let title = document.createElement("h3");
-        title.innerText = video.snippet.title;
-        info.appendChild(title);
-
-        let date = document.createElement("p");
-        date.innerText = new Date(video.snippet.publishedAt).toLocaleDateString('en-us', {year:"numeric", month:"short", day:"numeric"});
-        info.appendChild(date);
-
-        let link = document.createElement("a");
-        link.href = `http://www.youtube.com/watch?v=${video.contentDetails.videoId}`;
-        link.classList.add("videolistitemlink");
-        info.appendChild(link);
-
-        document.getElementById("videolist").appendChild(div);
+        // Creates the list entry on the webpage.
+        createVideoNode(video);
     }
 }
 
@@ -73,7 +90,7 @@ function populateVideos(response) {
 function requestVideos(response) {
     let parsedResponse = JSON.parse(response.body);
     let videoIdList = "";
-    for(let video of parsedResponse.items){
+    for (let video of parsedResponse.items) {
         videoIdList = videoIdList + video.contentDetails.videoId + ",";
     }
 
@@ -103,7 +120,7 @@ function requestPlaylistItems(playlistId, pageToken = null) {
  * @param response The response given by channels.list.
  * @returns The channel's uploads playlist ID.
  */
-function getUploadsPlaylist(response){
+function getUploadsPlaylist(response) {
     return JSON.parse(response.body).items[0].contentDetails.relatedPlaylists.uploads;
 }
 
@@ -112,7 +129,7 @@ function getUploadsPlaylist(response){
  * items from the channel's uploads playlist.
  * @param response The response given by channels.list.
  */
-function handleChannelResponse(response){
+function handleChannelResponse(response) {
     requestPlaylistItems(getUploadsPlaylist(response));
 }
 
@@ -143,14 +160,14 @@ function changeTitle(videoId, newTitle) {
             "id": videoId,
             "snippet": snippet
         }
-    }).then(function (response) {console.log(response)}, logError);
+    }).then(function (response) { console.log(response) }, logError);
 }
 
 /**
  * Runs when the button in the control panel is pressed. Performs the necessary
  * setup to run an AB Test.
  */
-function beginABTest(){
+function beginABTest() {
     // TODO: Implement
 }
 
